ecfc15f5f8b51b877059f1932c0a8da7f730466d
Mario Carretero Berrocal <mariocb@gamma27.aulas.gsyc.urjc.es>
2016-10-10 13:04:29 +0200

    apartado 6 terminado

--- karaoke.py
+++ karaoke.py
@@ -3,50 +3,63 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-from smallsmilhandler import SmallSMILHandler
+import smallsmilhandler
 import sys
 import json
 import urllib.request
 
-parser = make_parser()
-SSHandler = SmallSMILHandler()
-parser.setContentHandler(SSHandler)
-misdatos = SSHandler.get_tags()
 
-def do_local(misdatos):
-    for datos in misdatos:
-        for dato in datos:
-            if datos[dato][:7] == 'http://':
-                file_name = datos[dato].split('/')[-1]
-                web = urllib.request.urlretrieve(datos[dato], file_name)
-                print(web)
+class KaraokeLocal():
 
-def __str__(misdatos):
-    for datos in misdatos:    # datos es cada diccionario por separado
-        linea = datos['etiqueta']    # inicializo la salida a la etiqueta
-        del datos['etiqueta']
-        for dato in datos:    #dato es cada atributo del diccionario
-            valor = datos[dato]
-            linea = linea + '\t' + dato + '=' + '"' + valor + '"' 
-        print(linea)
-        linea = ''
+    def init(self, fich):
+        parser = make_parser()
+        self.KHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(self.KHandler)
+        parser.parse(open(fich))
+        self.misdatos = self.KHandler.get_tags()
 
-def to_json(misdatos):
-    smilfile = sys.argv[1]
-    jsonfile = open(smilfile.split('.')[0] + '.json', 'w')
-    jsoncontent = json.dumps(misdatos)
-    jsonfile.write(jsoncontent)
+    def do_local(self):
+        for datos in self.misdatos:
+            for dato in datos:
+                if datos[dato][:7] == 'http://':
+                    file_name = datos[dato].split('/')[-1]
+                    web = urllib.request.urlretrieve(datos[dato], \
+                        file_name)
+                    print(web)
+
+    def __str__(self):
+        for datos in self.misdatos: # datos es cada diccionario por separado
+            tag = datos['etiqueta']
+            linea = tag + '\t'
+            for dato in datos:    #dato es cada atributo del diccionario
+                if datos[dato] != tag:
+                    valor = datos[dato]
+                    linea = linea + '\t' + dato + '=' + '"' + valor + '"' 
+            print(linea)
+            linea = ''
+
+    def to_json(self, smilfile):
+        jsonfile = open(smilfile.split('.')[0] + '.json', 'w')
+        jsoncontent = json.dumps(self.misdatos)
+        jsonfile.write(jsoncontent)
 
 
 if __name__ == "__main__":
+
+
+
     try:
         fichero = sys.argv[1]
     except IndexError:
         sys.exit('Usage: python3 karaoke.py file.smil')
+    
+    KLocal = KaraokeLocal()
 
-    parser.parse(open(fichero))
-    smil_to_json(misdatos)
-    #__str__(misdatos)
-    download(misdatos)
+    KLocal.init(fichero)
+    KLocal.__str__()
+    KLocal.to_json(fichero)
+    KLocal.do_local()
+    KLocal.to_json('local')
+    KLocal.__str__()
 
 
